7deb71776049c9280a03e0062499a8cdfb3b748e
Sultan <sbaratahunov@gmail.com>
2023-11-13 14:45:16 +0600

    ref

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,45 +1,66 @@
+import { Component } from "react";
 import "./employees-list-item.css";
 
-const EmployeesListItem = (props) => {
-  const { name, salary, onDelete, onToggleProp, increase, rise } = props;
-  let classNames = "list-group-item d-flex justify-content-between";
-  if (increase) {
-    classNames += " increase";
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      salary: props.salary,
+    };
   }
-  if (rise) {
-    classNames += " like";
-  }
-  return (
-    <li className={classNames}>
-      <span
-        className="list-group-item-label"
-        data-toggle="rise"
-        onClick={onToggleProp}
-      >
-        {name}
-      </span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + "$"}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button
-          type="button"
-          onClick={onToggleProp}
-          data-toggle="increase"
-          className="btn-cookie btn-sm "
-        >
-          <i className="fas fa-cookie"></i>
-        </button>
 
-        <button type="button" className="btn-trash btn-sm " onClick={onDelete}>
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+  onChangeSalary = (e) => {
+    const newSalary = e.target.value.replace(/\D/g, "");
+    this.setState({
+      salary: newSalary,
+    });
+    this.props.onChangeSalary(newSalary);
+  };
+  render() {
+    const { name, salary, onDelete, onToggleProp, increase, rise } = this.props;
+    let classNames = "list-group-item d-flex justify-content-between";
+    if (increase) {
+      classNames += " increase";
+    }
+    if (rise) {
+      classNames += " like";
+    }
+    return (
+      <li className={classNames}>
+        <span
+          className="list-group-item-label"
+          data-toggle="rise"
+          onClick={onToggleProp}
+        >
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + "$"}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            onClick={onToggleProp}
+            data-toggle="increase"
+            className="btn-cookie btn-sm "
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button
+            type="button"
+            className="btn-trash btn-sm "
+            onClick={onDelete}
+          >
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;
